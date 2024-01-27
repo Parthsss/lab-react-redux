86c394f0b2cc23f835497468d089d173d0f2e47f
Parthssh <naturallyinsane13@gmail.com>
2024-01-27 14:35:36 +0530

    added changes

---
+++ redux/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import LikeCounterApp from './LikeCounter'
+
+function App() {
+  
+
+  return (
+    <>
+      <LikeCounterApp/>
+    </>
+  )
+}
+
+export default App
